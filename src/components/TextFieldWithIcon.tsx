4115b594da5f24913f39005ebee17b6dacad6e7e
Juan Venegas <jvenegasb@estud.usfq.edu.ec>
2025-03-30 16:11:22 -0500

    Adjusted autoresize height on user input

--- src/components/TextFieldWithIcon.tsx
+++ src/components/TextFieldWithIcon.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Label, Textarea } from '@fluentui/react-components';
 
 interface TextFieldWithIconProps {
@@ -10,16 +10,27 @@ interface TextFieldWithIconProps {
 }
 
 export default function TextFieldWithIcon({ label, id, placeholder, handleDatachange, value }: TextFieldWithIconProps) {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        const textarea = textareaRef.current;
+        if (textarea) {
+            textarea.style.height = "auto"; 
+            textarea.style.height = `${textarea.scrollHeight}px`; 
+        }
+    }, [value]);
     return (
         <div className='flex flex-col'>
             {label && <Label htmlFor={id}>{label}</Label>}
             <Textarea
+                ref={textareaRef}
                 id={id}
                 name={id}
                 value={value}
                 placeholder={placeholder}
                 onChange={handleDatachange}
                 resize='vertical'
+                style={{ resize: "none", overflow: "hidden" }} 
             />
         </div>
     )
